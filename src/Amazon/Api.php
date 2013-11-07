cc535e938d09dc4f52fe72cdac82701cbb00d2a3
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2013-11-07 16:35:32 -0500

    Add Amazon

---
+++ src/Amazon/Api.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Amazon;
+
+use ApaiIO\Configuration\GenericConfiguration;
+use ApaiIO\Operations\Search;
+use ApaiIO\ApaiIO;
+
+
+class Api
+{
+    protected static $apaiIO;
+
+    
+    public function __construct($country, $access_key, $secret_key, $associate_tag)
+    {
+        
+        $conf = new GenericConfiguration();
+        $conf
+            ->setCountry($country)
+            ->setAccessKey($access_key)
+            ->setSecretKey($secret_key)
+            ->setAssociateTag($associate_tag);
+        $this->apaiIO = new ApaiIO($conf);
+    }
+    
+    public function searchByTitle($title)
+    {
+        $search = $this->createSearch();
+        $search->setTitle($title);
+        return $this->runOperation($search);
+    }
+    
+    public function searchByAuthor($author)
+    {
+        $search = $this->createSearch();
+        $search->setAuthor($author);
+        return $this->runOperation($search);
+    }
+    
+    protected function createSearch()
+    {
+        $search = new Search();
+        //$search->setCategory('KindleStore');
+        $search->setCategory('Books');
+        $search->setSort('salesrank');
+        return $search;
+    }
+    
+    protected function runOperation($op)
+    {
+        $formattedResponse = $this->apaiIO->runOperation($op);
+        $xml = @simplexml_load_string($formattedResponse);
+        $json = json_encode($xml);
+        $json = json_decode($json, true);
+        return $json;
+    }
+}
+
